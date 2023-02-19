7351dcb99756fbffb82a37bcc1cdf2ee90c997f7
caster25 <teerapst1236@gmail.com>
2023-02-19 17:02:48 +0700

    lc14

--- check.php
+++ check.php
@@ -1,3 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<style>
+    button[type=submit] {
+      background-color: #4CAF50;
+      border: none;
+      color: #FFFFFF;
+      padding: 10px 16px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      border-radius: 5px;
+      cursor: pointer;
+      margin-top: 20px;
+    }
+
+		body {
+			background-color: #F7F7F7;
+			font-family: Arial, sans-serif;
+			padding: 20px;
+		}
+		h1 {
+			color: #333333;
+			margin-bottom: 20px;
+			text-align: center;
+		}
+    h2 {
+			color: #333333;
+			margin-bottom: 20px;
+			text-align: center;
+		}
+		form {
+			background-color: #FFFFFF;
+			border: 1px solid #CCCCCC;
+			border-radius: 5px;
+			padding: 20px;
+			width: 400px;
+			margin: 0 auto;
+		}
+		label {
+			display: block;
+			margin-bottom: 10px;
+		}
+		input[type=text],
+		input[type=password] {
+			border: 1px solid #CCCCCC;
+			border-radius: 3px;
+			padding: 8px;
+			width: 100%;
+			box-sizing: border-box;
+			font-size: 14px;
+			margin-bottom: 20px;
+		}
+		input[type=submit] {
+			background-color: #4CAF50;
+			border: none;
+			color: #FFFFFF;
+			padding: 10px 16px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+			border-radius: 5px;
+			cursor: pointer;
+		}
+		input[type=submit]:hover {
+			background-color: #3E8E41;
+		}
+		button[type=submit] {
+			background-color: #4CAF50;
+			border: none;
+			color: #FFFFFF;
+			padding: 10px 16px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+			border-radius: 5px;
+			cursor: pointer;
+		}
+		button[type=submit]:hover {
+			background-color: #3E8E41;
+		}
+	</style>
+</head>
+<form>
 <?php
 require("connect_db.php");
 
@@ -21,17 +110,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: account.php?account_number=" . urlencode($account_number));
         exit();
     } else {
-        echo "Invalid username or password.";
+        echo "<h1>Invalid username or password.</h1>";
     }
 
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
 }
 ?>
+</form>
 
 <form method="post" action="home.php">
     <button type="submit">Black</button>
 </form>
+</html>
 
 
 
